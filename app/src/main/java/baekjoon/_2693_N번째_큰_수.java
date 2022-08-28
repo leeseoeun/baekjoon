9b18cc0f98994d823fed26ade8ac4837bb6cc0a5
leeseoeun <seoeun.shuen@gmail.com>
2022-08-28 23:00:52 +0900

    0828 1026 보물

---
+++ app/src/main/java/baekjoon/_2693_N번째_큰_수.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _2693_N번째_큰_수 {
+
+  public static void main(String[] args) throws NumberFormatException, IOException {
+
+    // 배열 입력
+    //    Scanner sc = new Scanner(System.in);
+    //    String str = sc.nextLine();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+
+    // 줄 개수
+    int number = Integer.parseInt(br.readLine());
+    for (int i = 0; i < number; i++) {
+
+      // String -> int
+      int[] numArr = new int[10];
+
+      String str = br.readLine();
+      String[] strArr = str.split(" "); // StringTokenizer
+      for (int j = 0; j < 10; j++) {
+        numArr[j] = Integer.parseInt(strArr[j]);
+      }
+
+      // 정답 출력
+      Arrays.sort(numArr);
+      // System.out.println(numArr[7]);
+      // bw.write(numArr[7]);
+      sb.append(numArr[7] + "\n");
+    }
+
+    System.out.println(sb);
+
+  }
+
+}
