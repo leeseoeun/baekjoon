a54e804fd5127c9a46938b6178e3f1de713e8a1d
leeseoeun <seoeun.shuen@gmail.com>
2022-08-29 21:27:53 +0900

    0829 2908 상수

---
+++ app/src/main/java/baekjoon/_2908_상수.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2908_상수 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // 수 거꾸로
+    StringBuffer sb = new StringBuffer(br.readLine());
+    String str = sb.reverse().toString();
+
+    // 수 비교
+    StringTokenizer st = new StringTokenizer(str);
+    int number = Integer.parseInt(st.nextToken());
+    int number2 = Integer.parseInt(st.nextToken());
+    if (number > number2) {
+      System.out.println(number);
+
+    } else {
+      System.out.println(number2);
+    }
+  }
+
+}
