5b336e4a6fca00951f7305d1b40c453927d6aa6a
leeseoeun <seoeun.shuen@gmail.com>
2022-08-30 22:17:24 +0900

    0830 1026 보물

--- app/src/main/java/baekjoon/_1026_보물.java
+++ app/src/main/java/baekjoon/_1026_보물.java
@@ -3,6 +3,9 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class _1026_보물 {
 
@@ -15,8 +18,32 @@ public class _1026_보물 {
 
     // 배열
     int[] arrA = new int[number];
-    int[] arrB = new int[number];
+    Integer[] arrB = new Integer[number];
 
+    for (int i = 0; i < 2; i++) {
+      String str = br.readLine();
+      StringTokenizer st = new StringTokenizer(str);
+
+      for (int j = 0; j < number; j++) {
+        if (i == 0) {
+          arrA[j] = Integer.parseInt(st.nextToken());
+
+        } else if (i == 1) {
+          arrB[j] = Integer.parseInt(st.nextToken());
+        }
+      }
+    }
+
+    // 배열 정렬
+    Arrays.sort(arrA);
+    Arrays.sort(arrB, Comparator.reverseOrder());   // Comparator 사용 시 int -> Integer
+
+    int answer = 0;
+    for (int i = 0; i < number; i++) {
+      answer += (arrA[i] * arrB[i]);
+    }
+
+    System.out.println(answer);
   }
 
 }
